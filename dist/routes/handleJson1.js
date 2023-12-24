0d5fa4371b7778b039d508b1b7af4fedc93bf5ed
Krish <krishrathor18@gmail.com>
2023-12-24 19:48:06 +0530

    added some more logic

--- dist/routes/handleJson1.js
+++ dist/routes/handleJson1.js
@@ -3,5 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.handleJson1 = void 0;
 const handleJson1 = (req, res) => {
     console.log('object');
+    console.log(req);
+    res.json({
+        message: 'recieved successfully'
+    });
 };
 exports.handleJson1 = handleJson1;
--- src/routes/handleJson1.ts
+++ src/routes/handleJson1.ts
@@ -1,4 +1,8 @@
 export const handleJson1 = (req: any, res: any) => {
     console.log('object');
     console.log(req);
+
+    res.json({
+        message: 'recieved successfully'
+    })
 }
